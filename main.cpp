404bc0deeb84338921991f5e8b8d783c99956462
ibsen4 <115497034+ibsen4@users.noreply.github.com>
2022-12-02 16:17:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
 int main(){
+  int n=0;
+    cin>>n;
+    if (numeroprimo(n)==true){
+        cout<<"numero primo"<<endl;
+    }
+    else
+        cout<<"numero non primo";
   return 0;
 }
