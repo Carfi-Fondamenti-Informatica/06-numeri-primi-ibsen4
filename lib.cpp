5162b16ead26e2abf0392fc608b423b0dc339476
ibsen4 <115497034+ibsen4@users.noreply.github.com>
2022-12-02 16:16:32 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
-
+#include "PrimaLibreria.h"
+bool numeroprimo(int num) {
+    int divisore = 2;
+    while (divisore < num) {
+        if (num%divisore==0) {
+            return false;
+        }
+        divisore++;
+    }
+    return true;
+}
